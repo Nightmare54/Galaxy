d7e937bf6216c2d35c518b9aa17046803e6e838e
codelegend <blazikenmask@gmail.com>
2014-09-29 01:02:26 +0530

    support lobby intro/desc

--- db_manager.js
+++ db_manager.js
@@ -185,6 +185,10 @@ Rooms.GlobalRoom.prototype.readChatrooms= function(firsttime){
 			continue;
 		}
 		var id = toId(addrooms[i].title);
+		if( id == 'lobby' ){
+			Rooms.lobby.introMessage = addrooms[i].introMessage || '';
+			Rooms.lobby.desc = addrooms[i].desc || '';
+		}
 		if( Rooms.rooms[id] )continue;
 		this.chatRoomData.push(addrooms[i]);
 		console.log("NEW CHATROOM: " + id);

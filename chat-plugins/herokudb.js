3857c0be02667826fb8767b3e1a93f5365fd7c61
codelegend <blazikenmask@gmail.com>
2014-09-29 00:20:11 +0530

    add /uploadrooms

--- chat-plugins/herokudb.js
+++ chat-plugins/herokudb.js
@@ -1,8 +1,17 @@
 /** heroku database management commands **/
 
 exports.commands = {
-  makesqlquery : function(target,room,user){
-		if( this.can('hotpatch') && target )
-		  DatabaseManager.Heroku.makeQuery(target,this);
+  	makesqlquery : function(target,room,user){
+		if( target && Config.devstaff(user.userid) >=0 )
+	  		DatabaseManager.Heroku.makeQuery(target,this);
+	},
+	uploadrooms : function(target,room,user){
+		if(!this.can('hotpatch')) return;
+	  	DatabaseManager.files.writeRoomData(this);
+	},
+	herokuhelp : function( target, room, user){
+		if(!this.can('hotpatch')) return;
+		this.sendReply('This server is running on heroku. Chatrooms.json must be updated manually, through pastebin,'
+		+'until another way is implemented...');
 	}
 };
